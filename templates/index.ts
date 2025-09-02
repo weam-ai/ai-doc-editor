e4196fcf876b76ca909eacd132d03c25486b11a3
Chirag Suthar <chirag@weam.ai>
2025-09-02 15:14:56 +0530

    removed unwanted templates

--- templates/index.ts
+++ templates/index.ts
@@ -1,21 +1,4 @@
 import { blankDocumentTemplate } from './blank-document';
-// import { emilyAndersonResumeTemplate } from './emily-anderson-resume';
-// import { sarahJohnsonResumeTemplate } from './sarah-johnson-resume';
-// import { jamesJohnsonResumeTemplate } from './james-johnson-resume';
-// import { marcusSterlingPortfolioTemplate } from './marcus-sterling-portfolio';
-// import { sarahMitchellPortfolioTemplate } from './sarah-mitchell-portfolio';
-// import { businessProposalTemplate } from './business-proposal';
-// import { technicalReportTemplate } from './technical-report';
-// import { meetingMinutesTemplate } from './meeting-minutes';
-// import { customerSuccessLeadTemplate } from './customer-success-lead';
-// import { dummyResumeTemplate } from './dummy-resume';
-// import { twoColumnResumeTemplate } from './two-column-resume';
-// import { twoColumnProfileTemplate } from './two-column-profile';
-// import { experienceLetterTemplate } from './experience-letter';
-import { annualReportTemplate } from './annual-report';
-import { htmlDocumentTemplate } from './html-document';
-import { digitalMarketingTemplate } from './digital-marketing';
-import { digitalMarketingAgencyTemplate } from './digital-marketing-agency';
 import { janeDoeResumeTemplate } from './jane-doe-resume';
 import { businessProposalTemplate2 } from './business-proposal2';
 import { q3BusinessPerformanceTemplate } from './q3-business-performance';
@@ -29,11 +12,7 @@ import { professionalResumeTemplate } from './professional-resume';
 // Export all templates as an array
 export const allTemplates = [
   blankDocumentTemplate,
-  //annualReportTemplate,
-  //htmlDocumentTemplate,
   janeDoeResumeTemplate,
-  //digitalMarketingTemplate,
-  //digitalMarketingAgencyTemplate,
   businessProposalTemplate2,
   q3BusinessPerformanceTemplate,
   projectStatusTemplate,
@@ -41,18 +20,5 @@ export const allTemplates = [
   marketingOnboardingTemplate,
   q2BusinessReportTemplate,
   modernStartupTemplate,
-  professionalResumeTemplate,
-  // emilyAndersonResumeTemplate,
-  // sarahJohnsonResumeTemplate,
-  // jamesJohnsonResumeTemplate,
-  // marcusSterlingPortfolioTemplate,
-  // sarahMitchellPortfolioTemplate,
-  // businessProposalTemplate,
-  // technicalReportTemplate,
-  // meetingMinutesTemplate,
-  // customerSuccessLeadTemplate,
-  // dummyResumeTemplate,
-  // twoColumnResumeTemplate,
-  // twoColumnProfileTemplate,
-  // experienceLetterTemplate,  
+  professionalResumeTemplate
 ];
